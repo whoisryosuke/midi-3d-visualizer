4effab8dad016c02797df08802a2bbe1b8d27d93
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-01-31 01:32:50 -0800

    :sparkles: P5JS cause why not?

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,14 @@ import MidiKeyboard from "./features/MidiKeyboard";
 import Scene from "./components/Scene/Scene";
 import Music from "./features/Music";
 import Keyboard from "./features/Keyboard";
+import P5Test from "./components/p5/P5Test/P5Test";
 
 export default function App() {
   return (
     <div className="App">
       <MidiKeyboard />
-      <Scene />
+      {/* <Scene /> */}
+      <P5Test />
       <Music />
       <Keyboard />
     </div>
